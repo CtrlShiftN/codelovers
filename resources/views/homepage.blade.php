c07ba1935d4d79102523c5c3b83aed5bc3debea3
CtrShiftN <karuyanakaido24@gmail.com>
2022-05-19 13:34:14 +0700

    Addign seeder Signed-off-by: CtrShiftN <karuyanakaido24@gmail.com>

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Codelovers')
+
+@section('navbar')
+    @parent
+
+    <p>Đây là navbar</p>
+@endsection
+
+@section('content')
+    <p>Đây là phần hiển thị nội dung chính của trang</p>
+@endsection
